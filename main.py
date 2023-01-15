72845960e779139c21253830ddafd1437ec51aad
AkThao <akaash.thao@gmail.com>
2023-01-15 18:59:14 +0000

    Sound effects, music, background stars, new artwork and various gameplay improvements

--- main.py
+++ main.py
@@ -14,27 +14,48 @@ class Game:
         self.init_game()
     
     def init_game(self):
-        self.pig = Actor("pig_round")
+        self.pig = Actor("pig_round_right")
         self.pig.pos = WIDTH / 2, HEIGHT - self.pig.height / 2 # Image origin is at the center of the image
+        self.pig_speed = 5
+        self.pig_direction = 1  # 1 for right, 0 for left
         
         self.blocks = []
-        self.block_interval = 1.5
+        self.block_interval = 0.75
+        self.block_falling_speed = 4
+
         self.score = 0
         self.game_over = False
 
         # Call the add_block method after every block_interval
-        clock.schedule_interval(self.add_block, self.block_interval)
+        clock.schedule_unique(self.add_block, self.block_interval)
+
+        self.create_stars()
+
+        music.play("space_music")
 
     def add_block(self):
         block = Actor("block")
         # New block falls from the top at a random horizontal position
         block.pos = random.randrange(block.width / 2, WIDTH - (block.width / 2)), 0 - (block.height / 2)
         self.blocks.append(block)
+        self.block_interval = random.random() * 1.5
+        clock.schedule_unique(self.add_block, self.block_interval)
+
+    def create_stars(self):
+        self.stars = []
+        
+        for i in range(100):
+            rand_x_pos = random.randint(0, WIDTH)
+            rand_y_pos = random.randint(0, HEIGHT)
+            rand_size = random.random() * 4
+            self.stars.append((rand_x_pos, rand_y_pos, rand_size))
 
     def reset_game(self):
         # Stop the game for 2.0 seconds to show a game over message then reset the game
         self.game_over = True
-        self.pig.image = "pig_square"
+        sounds.oink.play()
+        music.stop()
+        self.pig.image = "pig_square_right" if self.pig_direction == 1 else "pig_square_left"
         clock.unschedule(self.add_block)
         clock.schedule_unique(self.init_game, 2.0)
 
@@ -45,12 +66,19 @@ game = Game()
 # Used by pgzero
 def draw():
     screen.clear()
-    game.pig.draw()
+
+    for star in game.stars:
+        screen.draw.filled_circle((star[0], star[1]), star[2], (255, 255, 255))
+
     for block in game.blocks:
         block.draw()
+
+    game.pig.draw()
+
     screen.draw.text(f"Score: {game.score}", center=(WIDTH / 2, 40), fontname="bruce_forever", fontsize=40, color=(193, 127, 64), owidth=1, ocolor=(240, 240, 240))
     if game.game_over:
         screen.draw.text("Game over!", center=(WIDTH / 2, HEIGHT / 2), fontname="bruce_forever", fontsize=50, color=(193, 127, 64), owidth=1, ocolor=(240, 240, 240))
+
 
 
 # Used by pgzero
@@ -58,9 +86,13 @@ def update():
     if game.game_over == False:
         # Pig movement
         if keyboard.left:
-            game.pig.x -=4
+            game.pig.x -= game.pig_speed
+            game.pig_direction = 0
+            game.pig.image = "pig_round_left"
         elif keyboard.right:
-            game.pig.x += 4
+            game.pig.x += game.pig_speed
+            game.pig_direction = 1
+            game.pig.image = "pig_round_right"
 
         # Stop pig from going off screen
         if (game.pig.x + game.pig.width / 2) > WIDTH:
@@ -70,10 +102,12 @@ def update():
 
         # Update block position and block array
         for block in game.blocks:
-            block.y += 4
+            block.y += game.block_falling_speed
             if (block.y > HEIGHT + (block.height / 2)):
                 game.blocks.remove(block)
                 game.score += 1
+                if game.score % 10 == 0:
+                    sounds.get_point.play()
 
         # collidelist stores a list of collisions, returns the index of the first collision and returns -1 if there are no collisions
         collision = game.pig.collidelist(game.blocks)
